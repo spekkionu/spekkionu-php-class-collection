e2e3cd5428cde699637151510eb1d9622a9316b6
spekkionu <spekkionu@spekkionu.com>
2009-09-29 18:25:04 -0700

    Added file and image upload classes

---
+++ lib/Upload/File.php
@@ -0,0 +1,275 @@
+<?php
+/**
+ * File Upload Class
+ *
+ * @author Jonathan Bernardi
+ * @copyright 2008
+ * @version 1.1 5/28/2008
+ * @package Upload
+ */
+/**
+ * File Upload Class
+ */
+class Upload_File {
+  /**
+   * @var string $_file The file to upload.
+   */
+  private $_file;
+  /**
+   * @var string $_dir The directory to upload the file to.
+   */
+  private $_dir;
+  /**
+   * @var string $_oldfilename The desired filename.
+   */
+  private $_oldfilename;
+  /**
+   * @var string $_newfilename The final saved filename.
+   */
+  private $_newfilename;
+  /**
+   * @var string $_overmethod What to do if file already exists in upload directory.
+   */
+  private $_overmethod = "over";
+  /**
+   * @var string|array $_extensions An extension or array of extensions that are acceptable.
+   */
+  private $_extensions = array();
+  /**
+   * @var string $_fileext The file extension.
+   */
+  private $_fileext;
+  /**
+   * @var string $_maxsize The max filesize allowed.
+   */
+  private $_maxsize;
+  /**
+   * @var string $_uffixtype Whether to append the uffix to beginning or end of filename.
+   */
+  private $_uffixtype = "";
+  /**
+   * @var string $_uffix The string to append to the filename.
+   */
+  private $_uffix = "";
+  /**
+   * Class Constructor
+   * @access public
+   * @param string $file A pointer to the uploaded file.  Can be absolute or relative path or from the $_FILES superglobal
+   * @param string $dir The directory to upload the file to. Can be absolute or relative path.
+   * @param string $filename The desired filename.
+   * @return Upload_File
+   */
+  public function __construct($file, $dir, $filename) {
+    $this->_setFile($file);
+    $this->_setFileName($filename);
+    $this->_setUploadPath($dir);
+    $this->_maxsize = 1000000 * intval(ini_get('upload_max_filesize'));
+  }
+  /**
+   * Sets the path to to the upload directory.
+   * @access private
+   * @param string $dir The desired upload directory
+   */
+  private function _setUploadPath($dir) {
+    $dir = trim($dir);
+    if (!is_dir($dir)) {
+      throw new Exception("Upload Path doesn't exist.");
+    }
+    if (!is_writable($dir)) {
+      throw new Exception("Upload Path isn't writable.");
+    }
+    if (substr($dir, -1) == "/") $dir = rtrim($dir, "/");
+    $this->_dir = $dir;
+  }
+  /**
+   * Sets the desired filename
+   * @access private
+   * @param string $name The desired filename.
+   */
+  private function _setFileName($name) {
+    $name = basename(trim($name));
+    $name = str_replace(" ", "_", $name);
+    $this->_oldfilename = $name;
+    $name = explode(".", $name);
+    if (count($name) <= 1) {
+      throw new Exception("Filename has no extension.");
+    }
+    $this->_fileext = strtolower(trim(array_pop($name)));
+  }
+  /**
+   * Sets the file resource
+   * @access private
+   * @param string $file The file resource.
+   */
+  private function _setFile($file) {
+    if (!is_uploaded_file($file)) {
+      if (!is_file($file)) {
+        throw new Exception("Not a valid file.");
+      }
+    }
+    $this->_file = $file;
+  }
+  /**
+   * Sets allowed extensions for uploaded file.
+   * @access public
+   * @param string|array $ext An acceptable extension or array of accepted extensions.
+   * @return Upload_File
+   */
+  public function setExtensions($ext) {
+    if (!is_array($ext)) $ext = explode("|", $ext);
+    foreach($ext as $value) {
+      $value = trim($value);
+      $value = str_replace(array("*", "."), "", $value);
+      $this->_extensions[] = $value;
+    }
+    return $this;
+  }
+  /**
+   * Checks the file extension against list of acceptable extensions.
+   * If Upload_File::setExtensions() is not called will always return true
+   * @access private
+   * @return boolean
+   */
+  private function _checkExtensions() {
+    if (count($this->_extensions) == 0) {
+      return true;
+    } else {
+      return in_array($this->_fileext, $this->_extensions);
+    }
+  }
+  /**
+   * Sets the max filesize.
+   * If larger than the php upload_max_filesize the php.ini setting will be used instead.
+   * @access public
+   * @return Upload_File
+   */
+  public function setMaxSize($size) {
+    $this->_maxsize = min($size, 1000000 * intval(ini_get('upload_max_filesize')));
+    return $this;
+  }
+  /**
+   * Sets an uffix to append to the beginning or end of the filename.
+   * @access public
+   * @param string $uffix The uffic to append.
+   * @param string $type Can be "pre" or "suff" This tells whether to append to the beginning or end of the filename.
+   * @return Upload_File
+   */
+  public function setUffix($uffix, $type = "pre") {
+    $uffix = trim($uffix);
+    $type = trim($type);
+    switch ($type) {
+      case "pre":
+        $this->_uffixtype = "pre";
+      break;
+      case "suff":
+        $this->_uffixtype = "suff";
+      break;
+      default:
+        throw new Exception("Not a valid uffix type.");
+      break;
+    }
+    $this->_uffix = $uffix;
+    return $this;
+  }
+  /**
+   * Sets what to do if the uploaded file already exists in the upload directory.
+   * @access public
+   * @param striong $type Can be "overwrite", "unique", or "error".
+   *   "overwrite" will overwrite the existing file.
+   *   "unique" will append _n to the uploaded filename until it is unique.
+   *   "error" will throw an exception of the file already exists.
+   * @return Upload_File
+   */
+  public function setIfExists($type = "over") {
+    $type = trim($type);
+    switch ($type) {
+      case "overwrite":
+        $this->_overmethod = "over";
+      break;
+      case "over":
+        $this->_overmethod = "over";
+      break;
+      case "unique":
+        $this->_overmethod = "unique";
+      break;
+      case "error":
+        $this->_overmethod = "error";
+      break;
+      default:
+        throw new Exception("Not a valid duplicate file method.");
+      break;
+    }
+    return $this;
+  }
+  /**
+   * Performs the file upload.
+   * Returns the new filename.
+   * @access public
+   * @return string The new filename.
+   */
+  public function performUpload() {
+    $this->_newfilename = $this->_checkFileName();
+    if (!$this->_checkExtensions()) {
+      throw new Exception("Not an acceptable extension.");
+    }
+    if (is_uploaded_file($this->_file)) {
+      if (move_uploaded_file($this->_file, $this->_dir . "/" . $this->_newfilename)) {
+        return $this->_newfilename;
+      } else {
+        throw new Exception("Error saving file.");
+      }
+    } else {
+      if (rename($this->_file, $this->_dir . "/" . $this->_newfilename)) {
+        return $this->_newfilename;
+      } else {
+        throw new Exception("Error saving file.");
+      }
+    }
+  }
+  /**
+   * Checks the filename to make sure it doesn't exists.
+   * If it does exist $Upload_File::_overmethod varibable determines action.
+   * @access private
+   * @return string New filename.
+   */
+  private function _checkFileName() {
+    $filename = explode(".", $this->_oldfilename);
+    $ext = $this->_fileext;
+    array_pop($filename);
+    $oldname = implode(".", $filename);
+    if ($this->_uffix != "") {
+      if ($this->_uffixtype == "pre") {
+        $oldname = $this->_uffix . $oldname;
+      } elseif ($this->_uffixtype == "suff") {
+        $oldname = $oldname . $this->_uffix;
+      }
+    }
+    $name = $oldname . "." . $ext;
+    if (!file_exists($this->_dir . "/" . $name)) {
+      //If it doesn't return the filename.
+      return $name;
+    } else {
+      switch ($this->_overmethod) {
+        case "over":
+          return $name;
+        break;
+        case "unique":
+          //Create a unique filename.
+          $j = 1;
+          do {
+            $fn = $oldname . "_" . $j;
+            $j++;
+          }
+          while (file_exists($this->_dir . "/" . $fn . "." . $ext));
+          return $fn . "." . $ext;
+          break;
+        case "error":
+          throw new Exception("File already exists and duplicate method set to error.");
+          break;
+        default:
+          throw new Exception("Incorrect overwrite method.");
+          break;
+        }
+    }
+  }
+}
---
+++ lib/Upload/Image.php
@@ -0,0 +1,448 @@
+<?php
+/**
+ * Image Upload and resize class
+ *
+ * @author Jonathan Bernardi
+ * @copyright 2008 Jonathan Bernardi<spekkionu@spekkionu.com>
+ * @version 1.0 06/24/2008
+ * @package Upload
+ */
+
+/**
+ * Upload_Image Class
+ *
+ */
+class Upload_Image {
+	
+	private $_file;
+	private $_output = array();
+	private $_imagetype;
+	private $_oldsize = array(
+		"width"  => 0,
+		"height" => 0
+	);
+	private $_maxsize;
+	private $_newsize = array();
+	private $_newfilename = array();
+	private $_resize = true;
+	private $_keepaspect = true;
+	private $_overmethod = "overwrite";
+	private $_complete = array();
+	private $_tmpimg;
+	
+	/**
+	 * Class constructor.
+	 * @param string $file The file to be uploaded.  Can be a relative or absolute path.
+	 */
+	public function __construct($file){
+		if(!is_file($file) or !is_readable($file)){
+			throw new Exception("File does not exist or is not readable.");
+		}
+		$this->_file = $file;
+		$this->_maxsize = intval(ini_get('upload_max_filesize'))*1000000;
+		$this->_imagetype = $this->_checkFileType();
+		$size = getimagesize($file);
+		
+		$this->_oldsize = array(
+			"width"  => $size[0],
+			"height" => $size[1]
+		);
+		
+	}
+	
+	/**
+	 * Adds an output file.
+	 * @param string $path The path to the directory to upload the file.
+	 * @param string $filename The desired filename for the output file.  If empty the original filename will be used.
+	 * @param integer $maxwidth The largest width allowed.  If zero will not be checked.
+	 * @param integer $maxheight The largest height allowed.  If zero will not be checked.
+	 * @param integer $minwidth The smallest width allowed.  If zero will not be checked.
+	 * @param integer $minheight The smallest height allowed.  If zero will not be checked.
+	 * @return Upload_Image
+	 */
+	public function addOutput($path = "./", $filename = "", $maxwidth = 0, $maxheight = 0, $minwidth = 0, $minheight = 0){
+		if(!is_dir($path)){
+			throw new Exception("$path is not an existing directory.");
+		}
+		if(!is_writable($path)){
+			throw new Exception("$path is not writable. Check permissions.");
+		}
+		$filename = trim($filename);
+		if($filename == ""){
+			$fname = basename($this->_file);
+		}
+		$filename = str_replace(" ","_",$filename);
+		$maxwidth = intval($maxwidth);
+		$maxheight = intval($maxheight);
+		$minwidth = intval($minwidth);
+		$minheight = intval($minheight);
+		if($maxwidth > 0 and ($minwidth > $maxwidth)){
+			throw new Exception("Min width cannot be greater than max width");
+		}
+		if($maxheight > 0 and ($minheight > $maxheight)){
+			throw new Exception("Min height cannot be greater than max height");
+		}
+		$this->_output[] = array(
+			"path"      => $path,
+			"filename"  => $filename,
+			"maxwidth"  => $maxwidth,
+			"maxheight" => $maxheight,
+			"minwidth"  => $minwidth,
+			"minheight" => $minheight
+		);
+		return $this;
+	}
+	
+	/**
+	 * Sets if the image will be resized.
+	 *
+	 * @param boolean $doresize If true image will be resized.  Otherwise exceptions will be thrown if image does not fit max and min dimensions.
+	 * If not called will be true.
+	 * @param boolean $keepaspect If true aspect ration will be kept when resizing.  If this isn't possible an exception will be thrown.  If doresize is false this isn't used.
+	 * @return Upload_Image
+	 */
+	public function setResizeOption($doresize = true, $keepaspect = true){
+		$this->_resize = ($doresize)?true:false;
+		$this->_keepaspect = ($keepaspect)?true:false;
+		return $this;
+	}
+	
+	/**
+	 * Sets what to do if file already exists.
+	 *
+	 * @param string $method can be overwrite, unique, or error.
+	 * If overwrite existing file will be overwritten with new file.
+	 * If unique _n will be appended to the filename until the filename is unique.
+	 * If error an exception will be thrown.
+	 * If not called overwrite will be used.
+	 * @return Upload_Image
+	 */
+	public function overwriteMethod($method = "overwrite"){
+		$method = strtolower(trim($method));
+		switch($method){
+			case "overwrite":
+				$this->_overmethod = "overwrite";
+				break;
+			case "unique":
+				$this->_overmethod = "unique";
+				break;
+			case "error":
+				$this->_overmethod = "error";
+				break;
+			default:
+				throw new Exception("Incorrect parameter for overwriteMethod(). Must be overwrite, unique, or error.");
+				break;
+		}
+		return $this;
+	}
+	
+	/**
+	 * Sets the maximum filesize of the uploaded file.
+	 * If not called upload_max_filesize ini setting is used.
+	 * Set a lower number if memory keeps running out.
+	 *
+	 * @param integer $size The max filesize in bytes.
+	 * @return Upload_Image
+	 */
+	public function setMaxFileSize($size){
+		$size = intval($size);
+		$this->_maxsize = min($size,1000000 * intval(ini_get('upload_max_filesize')));
+		return $this;
+	}
+	
+	/**
+	 * Performs the file upload and processing.
+	 *
+	 * @return array Returns a miltidimensional array with an index for each output file with width, height, and filename.
+	 */
+	public function process(){
+		if(count($this->_output) == 0){
+			throw new Exception("Now output files set.  Call addOutput() first");
+		}
+		if(filesize($this->_file) > $this->_maxsize){
+			throw new Exception("File is too large.");
+		}
+		foreach($this->_output as $key=>$output){
+			$this->_complete[$key]['filename'] = $this->_getNewFilename($key);
+			$size = $this->_calculateSize($key);
+			$this->_complete[$key]['width'] = $size['width'];
+			$this->_complete[$key]['height'] = $size['height'];
+			$this->_saveImage($key);
+		}
+		@imagedestroy($this->_tmpimg);
+		return $this->_complete;
+	}
+	
+	private function _getNewFilename($key){
+		$file = $this->_output[$key];
+		$oldfilename = $file['filename'];
+		$info = pathinfo($oldfilename);
+		// Check if filename already exists.
+		if(!file_exists($file['path'] . "/" . $oldfilename)){
+			$this->_newfilename[$key] = $oldfilename;
+			return $oldfilename;
+		}
+		switch($this->_overmethod){
+			case "overwrite":
+				$this->_newfilename[$key] = $oldfilename;
+				return $oldfilename;
+				break;
+			case "unique":
+				//Create a unique filename.
+				$j = 1;
+				do{
+					$fn = $info['filename'] . "_" . $j;
+					$j++;
+					$name = $fn . "." . $info['extension'];
+				}while(file_exists($file['path'] . "/" . $name));
+				$this->_newfilename[$key] = $name;
+				return $name;
+				break;
+			case "error":
+				throw new Exception("File already exists and duplicate method set to error.");
+				break;
+			default:
+				throw new Exception("Incorrect parameter for overwriteMethod(). Must be overwrite, unique, or error.");
+				break;
+		}
+	}
+	
+	private function _saveImage($key){
+		$output = $this->_output[$key];
+		$type = $this->_imagetype;
+		$size = $this->_newsize[$key];
+		switch($type){
+			case "gif":
+				$im = imagecreatefromgif($this->_file);
+				break;
+			case "jpg":
+				$im = imagecreatefromjpeg($this->_file);
+				break;
+			case "bmp":
+				$im = imagecreatefromwbmp($this->_file);
+				break;
+			case "png":
+				$im = imagecreatefrompng($this->_file);
+				break;
+			default:
+				throw new Exception("Incorrect image type.");
+				break;
+		}
+		if(!$im){
+			throw new Exception("Could not open image file.");
+		}
+		$newimage = imagecreatetruecolor($size['width'],$size['height']);
+		if(!$newimage){
+			throw new Exception("Unable to create new image.");
+		}
+		$white = imagecolorallocate($newimage, 255, 255, 255);
+		imagefill($newimage, 0, 0, $white);
+		if(!imagecopyresampled($newimage,$im,0,0,0,0,$size['width'],$size['height'],$this->_oldsize['width'],$this->_oldsize['height'])){
+			throw new Exception("Unable to copy image.");
+		}
+		imagedestroy($im);
+		$filename = $output['path'] . "/" . $this->_newfilename[$key];
+		switch($type){
+			case "gif":
+				if(!imagegif($newimage,$filename)){
+					throw new Exception("Unable to save new image.");
+				}
+				break;
+			case "jpg":
+				if(!imagejpeg($newimage,$filename,100)){
+					throw new Exception("Unable to save new image.");
+				}
+				break;
+			case "bmp":
+				if(!image2wbmp($newimage,$filename)){
+					throw new Exception("Unable to save new image.");
+				}
+				break;
+			case "png":
+				if(!imagepng($newimage,$filename)){
+					throw new Exception("Unable to save new image.");
+				}
+				break;
+			default:
+				throw new Exception("Incorrect image type.");
+				break;
+		}
+		imagedestroy($newimage);
+	}
+	
+	private function _calculateSize($key){
+		$file = $this->_output[$key];
+		$toolarge = false;
+		$toosmall=false;
+		$widthlarge = false;
+		$heightlarge = false;
+		$widthsmall = false;
+		$heightsmall = false;
+		$newwidth = $this->_oldsize['width'];
+		$newheight = $this->_oldsize['height'];
+		if($file['maxwidth'] and $this->_oldsize['width'] > $file['maxwidth']){
+			//width is too large
+			$toolarge = true;
+			$widthlarge = true;
+			if(!$this->_resize) throw new Exception("The image is too large.");
+		}
+		if($file['maxheight'] and $this->_oldsize['height'] > $file['maxheight']){
+			//height is too large
+			$toolarge = true;
+			$heightlarge = true;
+			if(!$this->_resize) throw new Exception("The image is too large.");
+		}
+		if($file['minwidth'] and $this->_oldsize['width'] < $file['minwidth']){
+			//width is too small
+			$toosmall = true;
+			$widthsmall = true;
+			if(!$this->_resize) throw new Exception("The image is too small.");
+		}
+		if($file['minheight'] and $this->_oldsize['height'] < $file['minheight']){
+			//height is too small
+			$toosmall = true;
+			$heightsmall = true;
+			if(!$this->_resize) throw new Exception("The image is too small.");
+		}
+		if($this->_keepaspect){
+			if($toolarge and $toosmall){
+				throw new Exception("Impossible to find correct height and width.");
+			}
+			if($toolarge){
+				if($widthlarge and $heightlarge){
+					//Both width and height are too large
+					//determine which which one is off by more.
+					$widthratio = $this->_oldsize['width'] / $file['maxwidth'];
+					$heightratio = $this->_oldsize['height'] / $file['maxheight'];
+					$ratio = $this->_oldsize['width'] / $this->_oldsize['height'];
+					if($widthratio >= $heightratio){
+						//width is bigger
+						$newwidth = $file['maxwidth'];
+						$newheight = ($this->_oldsize['height'] * $newwidth) / $this->_oldsize['width'];
+						//check if height is big enough.
+						if($newheight < $file['minheight']){
+							throw new Exception("Impossible to find correct height and width.");
+						}
+					}else{
+						//height is bigger
+						$newheight = $file['maxheight'];
+						$newwidth = ($this->_oldsize['width'] * $newheight) / $this->_oldsize['height'];
+						//check if width is big enough.
+						if($newwidth < $file['minwidth']){
+							throw new Exception("Impossible to find correct height and width.");
+						}
+					}
+				}else{
+					if($widthlarge){
+						//only width too large
+						$widthratio = $this->_oldsize['width'] / $file['maxwidth'];
+						$newwidth = $file['maxwidth'];
+						$newheight = ($this->_oldsize['height'] * $newwidth) / $this->_oldsize['width'];
+						//check if height is big enough.
+						if($newheight < $file['minheight']){
+							throw new Exception("Impossible to find correct height and width.");
+						}
+					}elseif($heightlarge){
+						//only height too large
+						$heightratio = $this->_oldsize['height'] / $file['maxheight'];
+						$newheight = $file['maxheight'];
+						$newwidth = ($this->_oldsize['width'] * $newheight) / $this->_oldsize['height'];
+						//check if width is big enough.
+						if($newwidth < $file['minwidth']){
+							throw new Exception("Impossible to find correct height and width.");
+						}
+					}
+				}
+			}elseif($toosmall){
+				if($widthsmall and $heightsmall){
+					//Both width and height are too small
+					//determine which which one is off by more.
+					$widthratio = $file['minwidth'] / $this->_oldsize['width'];
+					$heightratio = $file['minheight'] / $this->_oldsize['height'];
+					$ratio = $this->_oldsize['width'] / $this->_oldsize['height'];
+					if($widthratio <= $heightratio){
+						//width is bigger
+						$newwidth = $file['minwidth'];
+						$newheight = ($this->_oldsize['height'] * $newwidth) / $this->_oldsize['width'];
+						//check if height is small enough.
+						if($newheight > $file['maxheight']){
+							throw new Exception("Impossible to find correct height and width.");
+						}
+					}else{
+						//height is bigger
+						$newheight = $file['minheight'];
+						$newwidth = ($this->_oldsize['width'] * $newheight) / $this->_oldsize['height'];
+						//check if width is small enough.
+						if($newwidth > $file['maxwidth']){
+							throw new Exception("Impossible to find correct height and width.");
+						}
+					}
+				}else{
+					if($widthsmall){
+						//only width too small
+						$widthratio = $file['minwidth'] / $this->_oldsize['width'];
+						$newwidth = $file['minwidth'];
+						$newheight = ($this->_oldsize['height'] * $newwidth) / $this->_oldsize['width'];
+						//check if height is small enough.
+						if($newheight > $file['maxheight']){
+							throw new Exception("Impossible to find correct height and width.");
+						}
+					}
+					if($heightsmall){
+						//only height too small
+						$heightratio = $file['minheight'] / $this->_oldsize['height'];
+						$newheight = $file['minheight'];
+						$newwidth = ($this->_oldsize['width'] * $newheight) / $this->_oldsize['height'];
+						//check if width is small enough.
+						if($newwidth > $file['maxwidth']){
+							throw new Exception("Impossible to find correct height and width.");
+						}
+					}
+				}
+			}
+		}else{
+			if($widthlarge){
+				$newwidth = $file['maxwidth'];
+			}
+			if($heightlarge){
+				$newheight = $file['maxheight'];
+			}
+			if($widthsmall){
+				$newwidth = $file['minwidth'];
+			}
+			if($heightsmall){
+				$newheight = $file['minheight'];
+			}
+		}
+		$this->_newsize[$key] = array(
+			"width"  => $newwidth,
+			"height" => $newheight
+		);
+		return $this->_newsize[$key];
+	}
+	
+	private function _getFilename(){
+		
+	}
+	
+	/**
+	 * Makes sure file is an image.
+	 *
+	 * @return boolean
+	 */
+	private function _checkFileType(){
+		$types = array(
+			"image/gif"           => "gif",
+			"image/jpeg"          => "jpg",
+			"image/pjpeg"         => "jpg",
+			"image/bmp"           => "bmp",
+			"image/x-windows-bmp" => "bmp",
+			"image/png"           => "png"
+		);
+		$type = getimagesize($this->_file);
+		if(!$type){
+			throw new Exception("Provided file is not an image.");
+		}
+		return $types[image_type_to_mime_type($type[2])];
+	}
+}
