52438c12d2f6d5e16df808674d27bcad68199ec7
spekkionu <spekkionu@spekkionu.com>
2010-07-28 10:49:36 -0700

    Added Zend_Cache wincache backend.

---
+++ lib/MimeType.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * MimeType Parsing Class
+ * @author Jonathan Bernardi
+ * @copyright 2009 spekkionu <spekkionu@spekkionu.com>
+ * @package MimeTypes
+ * @license MIT License
+ */
+class MimeType {
+	
+}
